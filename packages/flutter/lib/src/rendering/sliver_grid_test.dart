7d32f39b8ae280c50d4b0de09280832a22faf990
Santiago Pedroza <pedrozas@google.com>
2022-08-24 15:24:03 -0500

    Added more tests

--- packages/flutter/lib/src/rendering/sliver_grid_test.dart
+++ packages/flutter/lib/src/rendering/sliver_grid_test.dart
@@ -18,14 +18,16 @@ void main() {
         child: Center(child: Text('Item $index')),
       ),
     );
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: GridView.wrap(
-          children: children,
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: GridView.wrap(
+            children: children,
+          ),
         ),
       ),
-    ));
-    for (int i = 0; i < 9; i++) {
+    );
+    for (int i = 0; i < 10; i++) {
       expect(find.text('Item $i'), findsOneWidget);
     }
   });
@@ -47,7 +49,10 @@ void main() {
           body: GridView.builder(
               itemCount: children.length,
               gridDelegate: const SliverGridDelegateWithWrapping(),
-              itemBuilder: (BuildContext context, int index) =>
+              itemBuilder: (
+                BuildContext context,
+                int index,
+              ) =>
                   children[index]),
         ),
       ),
@@ -55,111 +60,19 @@ void main() {
     for (int i = 0; i < 5; i++) {
       expect(find.text('Item $i'), findsOneWidget);
     }
+
     expect(find.text('Item 5'), findsNothing);
     await tester.scrollUntilVisible(find.text('Item 19'), 500.0);
     await tester.pumpAndSettle();
     expect(find.text('Item 18'), findsOneWidget);
     expect(find.text('Item 0'), findsNothing);
     expect(find.text('Item 1'), findsNothing);
+    expect(find.text('Item 2'), findsNothing);
+    expect(find.text('Item 3'), findsNothing);
   });
-  // TODO(snat-s): create this test
-  // testWidgets('Test for wrap if it wraps', (WidgetTester tester) async {
-  //   final List<Widget> children = List<Widget>.generate(
-  //     20,
-  //     (int index) => SizedBox(
-  //       height: index.isEven ? 100 : 50,
-  //       width: index.isEven ? 100 : 180,
-  //       child: Center(child: Text('Item $index')),
-  //     ),
-  //   );
-  //   // Change the size of the screen
-  //   tester.binding.window.physicalSizeTestValue = Size(500, 100);
 
-  //   await tester.pumpWidget(
-  //     MaterialApp(
-  //       home: Scaffold(
-  //       body: GridView.builder(
-  //           itemCount: children.length,
-  //             gridDelegate: const SliverGridDelegateWithWrapping(),
-  //             itemBuilder: (BuildContext context, int index) => children[index]
-  //           ),
-  //       ),
-  //     ),
-  //   );
-  //   // await tester.pumpAndSettle();
-  //   for(int i = 0; i < 3; i++) {
-  //      expect(find.text('Item $i'), findsOneWidget);
-  //   }
-  //   expect(find.text('Item 4'), findsNothing);
-
-  //   // resets the screen to its original size after the test end
-  //   addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
-  // });
-//   // TODO(snat-s): create this test
-//   testWidgets('Test wrap to see nothing affected if random elements are deleted', (WidgetTester tester) async {
-//     final List<Widget> children = List<Widget>.generate(
-//       150,
-//       (int index) => Container(
-//         height: index.isEven ? 1000 : 50,
-//         width: index.isEven ? 95 : 180,
-//         child: Center(child: Text('Item $index')),
-//       ),
-//     );
-
-//     await tester.pumpWidget(
-//       MaterialApp(
-//         home: Scaffold(
-//           body: GridView.builder(
-//               gridDelegate: const SliverGridDelegateWithWrapping(),
-//               itemBuilder: (BuildContext context, int index) => children[index]),
-//         ),
-//       ),
-//     );
-//   });
-//   // TODO(snat-s): create this test
-//   testWidgets('Test wrap for layouts that go from left to right instead of right to left', (WidgetTester tester) async {
-//     final List<Widget> children = List<Widget>.generate(
-//       150,
-//       (int index) => Container(
-//         height: index.isEven ? 1000 : 50,
-//         width: index.isEven ? 95 : 180,
-//         child: Center(child: Text('Item $index')),
-//       ),
-//     );
-
-//     await tester.pumpWidget(
-//       MaterialApp(
-//         home: Scaffold(
-//           body: GridView.builder(
-//               gridDelegate: const SliverGridDelegateWithWrapping(),
-//               itemBuilder: (BuildContext context, int index) => children[index]),
-//         ),
-//       ),
-//     );
-//   });
-//   // TODO(snat-s): create this test
-//   testWidgets('Test wrap for layouts that go from left to right instead of right to left', (WidgetTester tester) async {
-//     final List<Widget> children = List<Widget>.generate(
-//       150,
-//       (int index) => Container(
-//         height: index.isEven ? 1000 : 50,
-//         width: index.isEven ? 95 : 180,
-//         child: Center(child: Text('Item $index')),
-//       ),
-//     );
-
-//     await tester.pumpWidget(
-//       MaterialApp(
-//         home: Scaffold(
-//           body: GridView.builder(
-//               gridDelegate: const SliverGridDelegateWithWrapping(),
-//               itemBuilder: (BuildContext context, int index) => children[index]),
-//         ),
-//       ),
-//     );
-//   });
-//   // TODO(snat-s): create this test
-testWidgets('Test for GridView.wrap to scrollDirection Axis.horizontal', (WidgetTester tester) async {
+  testWidgets('Test for GridView.wrap to scrollDirection Axis.horizontal',
+      (WidgetTester tester) async {
     final List<Widget> children = List<Widget>.generate(
       20,
       (int index) => SizedBox(
@@ -219,10 +132,15 @@ testWidgets('Test for GridView.wrap to scrollDirection Axis.horizontal', (Widget
       MaterialApp(
         home: Scaffold(
           body: GridView.builder(
-              reverse: true,
-              itemCount: children.length,
-              gridDelegate: const SliverGridDelegateWithWrapping(),
-              itemBuilder: (BuildContext context, int index) => children[index]),
+            reverse: true,
+            itemCount: children.length,
+            gridDelegate: const SliverGridDelegateWithWrapping(),
+            itemBuilder: (
+              BuildContext context,
+              int index,
+            ) =>
+                children[index],
+          ),
         ),
       ),
     );
@@ -230,7 +148,8 @@ testWidgets('Test for GridView.wrap to scrollDirection Axis.horizontal', (Widget
       expect(find.text('Item 0'), findsOneWidget);
     }
   });
-  testWidgets('Test GridView.wrap for GridView.reverse to true',
+
+  testWidgets('GridView.wrap for GridView.reverse to true',
       (WidgetTester tester) async {
     final List<Widget> children = List<Widget>.generate(
       20,
@@ -255,4 +174,189 @@ testWidgets('Test for GridView.wrap to scrollDirection Axis.horizontal', (Widget
       expect(find.text('Item 0'), findsOneWidget);
     }
   });
+
+  testWidgets('GridView.wrap dismiss keyboard onDrag test',
+      (WidgetTester tester) async {
+    final List<FocusNode> focusNodes =
+        List<FocusNode>.generate(50, (int i) => FocusNode());
+
+    await tester.pumpWidget(
+      textFieldBoilerplate(
+        child: GridView.extent(
+          padding: EdgeInsets.zero,
+          maxCrossAxisExtent: 300,
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          children: focusNodes.map((FocusNode focusNode) {
+            return Container(
+              height: 50,
+              color: Colors.green,
+              child: TextField(
+                focusNode: focusNode,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+
+    final Finder finder = find.byType(TextField).first;
+    final TextField textField = tester.widget(finder);
+    await tester.showKeyboard(finder);
+    expect(textField.focusNode!.hasFocus, isTrue);
+
+    await tester.drag(finder, const Offset(0.0, -40.0));
+    await tester.pumpAndSettle();
+    expect(textField.focusNode!.hasFocus, isFalse);
+  });
+
+  testWidgets('Test wrap in Axis.vertical direction',
+      (WidgetTester tester) async {
+    final List<Widget> children = List<Widget>.generate(
+      5,
+      (int index) => SizedBox(
+        height: index.isEven ? 100 : 50,
+        width: index.isEven ? 100 : 180,
+        child: Center(child: Text('Item $index')),
+      ),
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: GridView.builder(
+              itemCount: children.length,
+              gridDelegate: const SliverGridDelegateWithWrapping(),
+              itemBuilder: (BuildContext context, int index) =>
+                  children[index]),
+        ),
+      ),
+    );
+
+    // Change the size of the screen
+    await tester.binding.setSurfaceSize(const Size(500, 100));
+    await tester.pumpAndSettle();
+    for (int i = 0; i < 3; i++) {
+      expect(find.text('Item $i'), findsOneWidget);
+    }
+    expect(find.text('Item 3'), findsNothing);
+    expect(find.text('Item 4'), findsNothing);
+    await tester.binding.setSurfaceSize(const Size(560, 100));
+    await tester.pumpAndSettle();
+    expect(find.text('Item 3'), findsOneWidget);
+    expect(find.text('Item 4'), findsNothing);
+    await tester.binding.setSurfaceSize(const Size(280, 100));
+    await tester.pumpAndSettle();
+    expect(find.text('Item 0'), findsOneWidget);
+    expect(find.text('Item 1'), findsOneWidget);
+    expect(find.text('Item 2'), findsNothing);
+    expect(find.text('Item 3'), findsNothing);
+    expect(find.text('Item 4'), findsNothing);
+    // resets the screen to its original size after the test end
+    addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+  });
+
+  testWidgets('Test wrap in Axis.horizontal direction',
+      (WidgetTester tester) async {
+    final List<Widget> children = List<Widget>.generate(
+      5,
+      (int index) => SizedBox(
+        height: index.isEven ? 100 : 50,
+        width: index.isEven ? 100 : 180,
+        child: Center(child: Text('Item $index')),
+      ),
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: GridView.wrap(
+              scrollDirection: Axis.horizontal, children: children),
+        ),
+      ),
+    );
+
+    // Change the size of the screen
+    await tester.binding.setSurfaceSize(const Size(180, 150));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Item 0'), findsOneWidget);
+    expect(find.text('Item 1'), findsOneWidget);
+    expect(find.text('Item 2'), findsNothing);
+    expect(find.text('Item 3'), findsNothing);
+
+    await tester.binding.setSurfaceSize(const Size(180, 400));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Item 0'), findsOneWidget);
+    expect(find.text('Item 1'), findsOneWidget);
+    expect(find.text('Item 2'), findsOneWidget);
+    expect(find.text('Item 3'), findsOneWidget);
+    expect(find.text('Item 4'), findsOneWidget);
+
+    await tester.binding.setSurfaceSize(const Size(560, 100));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Item 0'), findsOneWidget);
+    expect(find.text('Item 1'), findsOneWidget);
+    expect(find.text('Item 2'), findsOneWidget);
+    expect(find.text('Item 3'), findsOneWidget);
+    expect(find.text('Item 4'), findsNothing);
+
+    // resets the screen to its original size after the test end
+    addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+  });
+  // TODO(snat-s): 'Test wrap to see nothing affected if random elements are deleted'
+}
+
+Widget textFieldBoilerplate({required Widget child}) {
+  return MaterialApp(
+    home: Localizations(
+      locale: const Locale('en', 'US'),
+      delegates: <LocalizationsDelegate<dynamic>>[
+        WidgetsLocalizationsDelegate(),
+        MaterialLocalizationsDelegate(),
+      ],
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: MediaQuery(
+          data: const MediaQueryData(size: Size(800.0, 600.0)),
+          child: Center(
+            child: Material(
+              child: child,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class MaterialLocalizationsDelegate
+    extends LocalizationsDelegate<MaterialLocalizations> {
+  @override
+  bool isSupported(Locale locale) => true;
+
+  @override
+  Future<MaterialLocalizations> load(Locale locale) =>
+      DefaultMaterialLocalizations.load(locale);
+
+  @override
+  bool shouldReload(MaterialLocalizationsDelegate old) => false;
+}
+
+class WidgetsLocalizationsDelegate
+    extends LocalizationsDelegate<WidgetsLocalizations> {
+  @override
+  bool isSupported(Locale locale) => true;
+
+  @override
+  Future<WidgetsLocalizations> load(Locale locale) =>
+      DefaultWidgetsLocalizations.load(locale);
+
+  @override
+  bool shouldReload(WidgetsLocalizationsDelegate old) => false;
 }
